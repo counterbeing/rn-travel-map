60213f6b6a5d02afe32af1a8d5aed006c5d4630a
Cory Logan <he@corylogan.com>
2019-01-26 13:51:27 -0700

    tinkering

---
+++ src/components/Map.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div style="height: 100px; overflow: auto;">
+      <p> Center is at {{ currentCenter }} and the zoom is: {{ currentZoom }} </p>
+    </div>
+    <l-map
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      style="height: 800px"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
+    >
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
+      />
+      <l-marker :lat-lng="withPopup">
+        <l-popup>
+          <div @click="innerClick">
+            I am a popup
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet
+            </p>
+          </div>
+        </l-popup>
+      </l-marker>
+      <l-marker :lat-lng="withTooltip">
+        <l-tooltip :options="{permanent: true, interactive: true}">
+          <div @click="innerClick">
+            I am a tooltip
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+        </l-tooltip>
+      </l-marker>
+    </l-map>
+  </div></template>
+
+<script>
+import L from 'leaflet';
+
+
+export default {
+  name: 'Example',
+  data() {
+    return {
+      zoom: 13,
+      center: L.latLng(47.413220, -1.219482),
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: L.latLng(47.413220, -1.219482),
+      withTooltip: L.latLng(47.414220, -1.250482),
+      currentZoom: 11.5,
+      currentCenter: L.latLng(47.413220, -1.219482),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5,
+      },
+    };
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    innerClick() {},
+  },
+}; </script>
+
+<style>
+@import 'http://cdn.leafletjs.com/leaflet-0.7.5/leaflet.css';
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Map from '@/components/Map';
+import Data from '@/components/Data';
 
 Vue.use(Router);
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Map',
+      component: Map,
+    },
+    {
+      path: '/data',
+      name: 'Data',
+      component: Data,
     },
   ],
 });
